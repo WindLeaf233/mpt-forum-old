24cfed00e55ff5272cb08f4e187e0bb14311443d
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-08 18:34:58 +0800

    fix: cross origin

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 解决跨域问题
+  // 解决跨域问题 (dev env)
   devServer: {
     proxy: {
       '/api': {
-        // target: 'http://127.0.0.1:2333',
-        target: 'http://gu.windleaf.ml:2333',
+        target: 'http://127.0.0.1:2333',
+        // target: 'http://gu.windleaf.ml:2333',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
